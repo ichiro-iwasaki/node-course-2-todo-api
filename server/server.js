4d17ea9b05ae64ce69068811c03e7d49559b4439
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-09-08 19:50:23 +0900

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -32,7 +40,7 @@ app.get('/todos/:id', (req, res) => {
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.status(400).send();
+      return res.status(404).send();
     }
 
     res.send({todo});
